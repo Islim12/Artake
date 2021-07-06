bf6e62cd886e59c9e2b0b31c2a14c24fef5c2e7b
Islim12 <islimakkaya@gmail.com>
2021-07-06 13:41:09 +0300

    Update

--- Artake/app/src/main/java/com/islimakkaya/artake/entity/Tutorial.kt
+++
@@ -1,9 +0,0 @@
-package com.islimakkaya.artake.entity
-
-import java.io.Serializable
-
-data class Tutorial(var tutorial_id: Int,
-                    var tutorial_name: String,
-                    var tutorial_description: String,
-                    var tutorial_image_name: String): Serializable {
-}
